33c5fe7400ce00fc7d083640deb09bc0a8325bbc
Pepê <81209437+Pepe-77777@users.noreply.github.com>
2023-07-15 08:14:11 -0300

    Delete AICustom.py

--- AICustom.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-from tkinter import filedialog as fd
-
-def open(v="8", file="file.png", mode=Image.BOX, open="false"):
-    c = Image.open(file)
-    d = c.resize((c.width * v, c.height * v), resample=mode)
-    filename = fd.asksaveasfilename(title='Save a file', defaultextension=".png", filetypes=(("Image File", "*.png"),("All Files", "*.*") ))
-    d.save(filename)
-    d.show()
